86de44edb0fdcfa1490f97dd952197bed4bd78f1
Emile Karacostas <emile.karacostas@proton.me>
2025-01-28 19:12:44 +0100

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Retour sur la page principale
 function goToIndex () {
     document.querySelector('#tickethack').addEventListener('click', function() {
-        
+        window.location.href = "index.html";
     });
 };
   
@@ -10,8 +10,7 @@ goToIndex();
 //Aller sur la page "Cart"
 function goToCart () {
     document.querySelector('#cart').addEventListener('click', function() {
-        console.log('Click detected!');
-        //window.location.href = "cart.html";
+        window.location.href = "cart.html";
     });
 };
   
@@ -20,7 +19,7 @@ goToCart();
 //Aller sur la page "Bookings"
 function goToBookings () {
     document.querySelector('#bookings').addEventListener('click', function() {
-        console.log('Click detected!');
+        window.location.href = "bookings.html";
     });
 };
   
@@ -29,13 +28,19 @@ goToBookings();
 //Moteur de recherche
 function searchTrip () {
     document.querySelector('#btn-search').addEventListener('click', function() {
-        console.log('Click detected:');
-        
-        // fetch('http://localhost:3000/trips')
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data);
-        // });
+        const departure = document.querySelector('#search-departure').value;       
+        const arrival = document.querySelector('#search-arrival').value;
+
+        fetch('http://localhost:3000/trips')
+            .then(response => response.json())
+            .then(data => {
+                if (departure === '' || arrival === '') {
+                    document.querySelector('#train-logo').src = './images/notfound.png';
+                    document.querySelector('p').textContent = 'No trip found.';
+                } else {
+                    document.querySelector('#right-box').innerHTML += /* ????? */;
+                };
+            });
     });
 };
   
