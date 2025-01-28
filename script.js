57ba8fb02465d74892faa62a2554507ac0b88261
Emile Karacostas <emile.karacostas@proton.me>
2025-01-28 14:44:17 +0100

    added more style, started script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//Retour sur la page principale
+
+//Aller sur la page "Cart"
+
+//Aller sur la page "Bookings"
+
+//Moteur de recherche
+function searchTrip () {
+    document.querySelector('#btn-search').addEventListener('click', function() {
+      
+      }
+    )
+};
+  
+searchTrip();
