f28d21f3c873e2c957809fa9d123f6e13e8f1cbc
Emile Karacostas <emile.karacostas@proton.me>
2025-01-28 16:14:50 +0100

    updated style.css, created cart.html and bookings.html

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 //Retour sur la page principale
+function goToIndex () {
+    document.querySelector('#tickethack').addEventListener('click', function() {
+        
+    });
+};
+  
+goToIndex();
 
 //Aller sur la page "Cart"
+function goToCart () {
+    document.querySelector('#cart').addEventListener('click', function() {
+        
+    });
+};
+  
+goToCart();
 
 //Aller sur la page "Bookings"
+function goToBookings () {
+    document.querySelector('#bookings').addEventListener('click', function() {
+        
+    });
+};
+  
+goToBookings();
 
 //Moteur de recherche
 function searchTrip () {
     document.querySelector('#btn-search').addEventListener('click', function() {
-      
-      }
-    )
+        
+    });
 };
   
 searchTrip();
