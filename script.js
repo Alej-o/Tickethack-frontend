bee82e31c0b482a3b226dad413e3e36a4e2fb619
Your name <Your email>
2025-01-29 11:57:15 +0100

    agathe

--- script.js
+++ script.js
@@ -25,32 +25,3 @@ function goToBookings () {
   
 goToBookings();
 
-//Moteur de recherche
-// function searchTrip () {
-//     document.querySelector('#btn-search').addEventListener('click', function() {
-//         const departure = document.querySelector('#search-departure').value;       
-//         const arrival = document.querySelector('#search-arrival').value;
-
-//         fetch('http://localhost:3000/trips')
-//             .then(response => response.json())
-//             .then(data => {
-//                 if (departure === '' || arrival === '') {
-//                     document.querySelector('#train-logo').src = './images/notfound.png';
-//                     document.querySelector('p').textContent = 'No trip found.'
-//                 } else {
-//                     document.querySelector('#right-box').innerHTML += /* ????? */;
-//                 };
-//             });
-//     });
-// };
-fetch('http://localhost:3000/trips')
-.then(response => response.json())
-.then(data => {
-    if(data.trips){
-        document.querySelector('').innerHTML += `
-        
-    `;
-    }
-
-})
-// dsearchTrip();
