c4a71664d7e8544d2c20049a6b3e4b5c84683060
Your name <Your email>
2025-01-29 16:41:32 +0100

    front

--- cart-script.js
+++ cart-script.js
@@ -21,41 +21,49 @@ function deployCart() {
     fetch('http://localhost:3000/carts')
         .then(response => response.json())
         .then(data => {
+            if (data.cart.length === 0) {
+                document.querySelector('.center-box-cart').innerHTML = "<h4>Votre panier est vide.</h4>";
+                return;
+            }
             let cartHTML = `
                 <div class="new-trip-box">
                     <h4>My cart</h4>
-                    <div id="new-trips">
+                    <div id="new-trip">
             `;
-            data.cart.forEach(cart => {
+
+            data.cart.forEach((cart, index) => {
                 cartHTML += `
                     <div class="trip-item">
                         <h4>${cart.trip.departure} > ${cart.trip.arrival}</h4>
                         <h4>${new Date(cart.trip.date).toLocaleString('fr-FR')}</h4>
                         <h4>€${cart.trip.price}</h4>
-                        <button type="button" class="btn-delete">X</button>
+                        <button type="button" id="btn-delete" data-index="${index}">X</button>
                     </div>
                 `;
             });
 
+            const totalPrice = data.cart.reduce((sum, cart) => sum + cart.trip.price, 0);
             cartHTML += `
                     </div>
                     <div id="total">
-                        <h4>Total: €${data.cart.reduce((sum, cart) => sum + cart.trip.price, 0)}</h4>
+                        <h4>Total: €${totalPrice}</h4>
                         <button type="button" id="btn-purchase">Purchase</button>
                     </div>
                 </div>
             `;
 
             document.querySelector('.center-box-cart').innerHTML = cartHTML;
-            document.querySelectorAll('.btn-delete').forEach((btn, index) => {
+
+            document.querySelectorAll('.btn-delete').forEach(btn => {
                 btn.addEventListener('click', function () {
-                    data.cart.splice(index, 1); // Supprime l'élément du tableau
+                    const index = this.getAttribute("data-index");
+                    data.cart.splice(index, 1); 
                     deployCart(); 
                 });
             });
 
         })
-        .catch(error => console.error("Erreur :", error));
+
 }
 
 deployCart();
